51f1dcc5b788359fe3a4b17b225448cdf7dea4b2
Shahriyar-Shazib <18-36610-1@student.aiub.edu>
2020-11-24 22:32:39 +0600

    Admin page Routing

--- Controller/Admin.js
+++ Controller/Admin.js
@@ -5,7 +5,19 @@ router.get('/', (req, res)=>{
 	res.render('Admin/Home')
 })
 
-
-
-
+router.get('/PendingPost', (req, res)=>{
+	res.render('Admin/PendingPost')
+})
+router.get('/PendingSignup', (req, res)=>{
+	res.render('Admin/PendingSignup')
+})
+router.get('/SCOUTlist', (req, res)=>{
+	res.render('Admin/SCOUTlist')
+})
+router.get('/userlist', (req, res)=>{
+	res.render('Admin/userlist')
+})
+router.get('/profile', (req, res)=>{
+	res.render('Admin/profile')
+})
 module.exports = router;
